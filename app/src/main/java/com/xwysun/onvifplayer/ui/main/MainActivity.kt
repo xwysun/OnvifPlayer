787e99126c7b414fa793916c4d09611f72161d34
xwysun <xwysun@qq.com>
2018-08-01 16:59:47 +0800

    add commom adapter

--- app/src/main/java/com/xwysun/onvifplayer/ui/main/MainActivity.kt
+++ app/src/main/java/com/xwysun/onvifplayer/ui/main/MainActivity.kt
@@ -2,9 +2,11 @@ package com.xwysun.onvifplayer.ui.main
 
 import android.os.Bundle
 import com.xwysun.onvifplayer.R
+import com.xwysun.onvifplayer.support.adapter.setUP
 import com.xwysun.onvifplayer.ui.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
 
 class MainActivity : BaseActivity(){
@@ -12,7 +14,9 @@ class MainActivity : BaseActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        btn_search.setOnClickListener({
+            toast("test")
+        })
     }
 }
 
