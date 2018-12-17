7ad96c789deca4a84379e69a4e8dd620ba6638eb
xwysun <xwysun@qq.com>
2018-12-17 17:33:41 +0800

    remove anko

--- app/src/main/java/com/xwysun/onvifplayer/support/adapter/CommonEx.kt
+++ app/src/main/java/com/xwysun/onvifplayer/support/adapter/CommonEx.kt
@@ -1,7 +1,6 @@
 package com.xwysun.onvifplayer.support.adapter
 
 import android.app.Activity
-import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
